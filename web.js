d5e335d29a547b1f8f772f844ff6689a8e7045b1
foxerlich@gmail.com <foxerlich@gmail.com>
2013-07-09 20:12:04 +0000

    Modified to read from index.html

--- web.js
+++ web.js
@@ -2,6 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = Buffer(256);
+
+buf = fs.readFileSync("/home/ubuntu/bitstarter/index.html");
 app.get('/', function(request, response) {
   response.send("Hello world3");
 });
