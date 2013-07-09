0fe63b3305442fb8f1921b59fd76ae87f4d5253c
foxerlich@gmail.com <foxerlich@gmail.com>
2013-07-09 21:09:02 +0000

    Modified to read from index.html

--- web.js
+++ web.js
@@ -2,11 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var fs = require('fs');
-fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
-  response.send("Hello world3");
+var fs = require('fs');
+var buf = new Buffer(256);
+buf = fs.readFileSync("index.html");
+  response.send(buf.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
