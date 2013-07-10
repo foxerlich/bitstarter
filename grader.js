0f020a6884bf1473d5ecc6387027cfb27e5fca7e
foxerlich@gmail.com <foxerlich@gmail.com>
2013-07-10 20:39:26 +0000

    Add grader.js

--- grader.js
+++ grader.js
@@ -1,3 +1,25 @@
+#!/usr/bin/env node
+/*
+Automatically grade files for the presence of the specified HTML tags/attribute.
+Uses commander.js and cheerio. Teaches command line application development and basic DOM parsing.
+
+References:
+
++ cheerio
+    - https://github.com/MatthewMueller/cheerio
+    - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+    - http://maxogden.com/scraping-with-node-html
+    
++ commander
+    - https://github.com/visionmedia/commander.js
+    - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+    
++ JSON
+    - http://en.wikipedia.org/wiki/JSON
+    - https://developer.mozilla.org/en-US/docs/JSON
+    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+    
+*/
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
@@ -12,13 +34,17 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlFile) {
+var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var loadChecks = function(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile))
+}
+
 var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlFile);
-    var checks = loadChecks(checkfile).sort();
+    $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
@@ -31,7 +57,7 @@ if(require.main == module) {
     program
         .option('-c, --checks', 'Path to checks.jsom', assertFileExists, CHECKSFILE_DEFAULT)
         .option('-f, --file','Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
-        .parse(process.arv);
+        .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
