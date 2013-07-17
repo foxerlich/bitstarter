4db0a679b5895b53e10e868711ed2be8e511c91e
foxerlich@gmail.com <foxerlich@gmail.com>
2013-07-17 18:47:20 +0000

    Open port 8080 web.js

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ buf = fs.readFileSync("index.html");
   response.send(buf.toString('utf8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
